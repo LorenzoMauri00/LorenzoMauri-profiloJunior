ed4262c06ec2e3d190687ca1b3028289f5539f14
LorenzoMauri00 <162593630+LorenzoMauri00@users.noreply.github.com>
2024-03-19 17:17:24 +0100

    Update Calcolatrice.java
    Aggiunti i commenti al file Calcolatrice.java

--- EsercizioCalcolatrice/src/esercizio/calcolatrice/Calcolatrice.java
+++ EsercizioCalcolatrice/src/esercizio/calcolatrice/Calcolatrice.java
@@ -6,7 +6,7 @@ public class Calcolatrice {
 
 	public static void main(String[] args) {
 			
-		//
+		//creo oggetto scanner e ricevo il numero dall'utente
 		Scanner scanner = new Scanner(System.in);
 		
 		System.out.println("Inserire un numero binario da convertire");
@@ -16,17 +16,17 @@ public class Calcolatrice {
 		int lunghezza = numeroBinario.length();
         int numeroDecimale = 0;
         
-        //
+        //cilo la stringa partendo dall'utlimo carattere
 		for (int i = 0; i < lunghezza; i++) {
 			
-            //
+            //ogni carattere viene convertito a valore int
             int bit = Character.getNumericValue(numeroBinario.charAt(lunghezza - 1 - i));
             
-            //
+            //conversione a decimale
             numeroDecimale += bit * Math.pow(2, i);
         }
 		
-		//
+		//stampo il risultato dopo la conversione
 		System.out.println("Il numero decimale corrispondente è: " + numeroDecimale);
 	}
 }
